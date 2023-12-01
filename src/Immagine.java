f5fd1eef42fa62c52be3925fd368aded76611bee
Riccardo Di Bari <riccardodibari@hotmail.it>
2023-12-01 16:15:16 +0100

    ho creato la classe Immagine

--- src/Immagine.java
+++ src/Immagine.java
@@ -1,2 +1,20 @@
-public class Immagine {
+public class Immagine  extends ElementoMultimediale{
+
+    private int lumonisita;
+
+    public Immagine(String titolo, int lumonisita) {
+        super(titolo);
+        this.lumonisita = lumonisita;
+    }
+
+    public void show() {
+        System.out.println(titolo + "*".repeat(lumonisita));
+    }
+
+    @Override
+    public void esegui() {
+        show();
+    }
 }
+
+
